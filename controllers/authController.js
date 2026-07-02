c1af70de7c5f29bce37758d899065acc1a03dec9
Rana Hammad <hammadiqbal3535@gmail.com>
2026-07-02 16:47:13 +0500

    Fix: use hashed password on register

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,7 +41,7 @@ const register = async (req, res) => {
     const user = await User.create({
       name,
       email: email.toLowerCase(),
-      password,
+      password: hashedPassword,
       cnic,
       phone,
     });
